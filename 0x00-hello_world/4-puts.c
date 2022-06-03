53541682e5ecca92b470c354f8c864d9f0b340f1
silvioukoth <silvioukoth@gmail.com>
2022-06-03 04:43:47 -0700

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
-#include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-}
+1 #include<stdio.h>
+2
+3 /**
+4 * main - Entry point
+5 *
+6 * Return: Always 0 (Success)
+7 */
+8 int main(void)
+9 {
+10   puts("\"Programming is like building a multilingual puzzle");
+12   return (0);
+12 }
