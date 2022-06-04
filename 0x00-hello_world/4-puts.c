b1219f635a4b86ef2e54b51cead185a50f0c40eb
silvioukoth <silvioukoth@gmail.com>
2022-06-04 03:26:30 -0700

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main ->  C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ * main -> C program to prints
 *
 * Return: Always 0 (Success)
 */
