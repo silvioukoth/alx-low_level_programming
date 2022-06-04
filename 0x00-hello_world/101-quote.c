d92ef40d748192da14d7c0367d3175042b6097a5
silvioukoth <silvioukoth@gmail.com>
2022-06-04 03:52:22 -0700

    add task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,5 +2,11 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* Return:1
+ * main -> C program to prints piece of art
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
