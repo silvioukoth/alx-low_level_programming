da3ececab6543fd2d8dfd11f4db54555f2c8ed57
silvioukoth <silvioukoth@gmail.com>
2022-06-11 00:43:08 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char word[9] = "_putchar"
-
-		int i = 0;
-	
-	while(i < 9)
-	{
-		_putchar(word[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return (0);
+
+	return(0);
 }
