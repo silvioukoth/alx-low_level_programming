8d233e288320c4ebdd493a378b7dbb20c8ea889c
silvioukoth <silvioukoth@gmail.com>
2022-06-11 02:29:48 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints_alphabets - entry point
+ * Description: show the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
+
 
 #endif
+
