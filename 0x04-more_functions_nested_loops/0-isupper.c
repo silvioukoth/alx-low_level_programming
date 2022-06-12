a71000aad1114e362e2703591b4760e3c4b9ec2b
silvioukoth <silvioukoth@gmail.com>
2022-06-12 05:57:17 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper -main entry.
+ * @c: integer to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	/*65 y 90 son los codigos ascci para A y Z */
+
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
