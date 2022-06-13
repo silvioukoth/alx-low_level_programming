0fa047848d522e7a8eae339eb4b8089c73261a2e
silvioukoth <silvioukoth@gmail.com>
2022-06-13 01:18:37 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 01234567891011121314\n x10
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
