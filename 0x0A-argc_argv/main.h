91de2509e818ebb7c7fef15c1a6b6cded2ed3dae
silvioukoth <silvioukoth@gmail.com>
2022-06-24 16:12:14 -0700

    task4

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,5 +6,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char __attribute__((unused)) argc, char *argv[]);
 
 #endif
