109ed9a2a18fa17cb5f45134a6f35cb7124beab0
silvioukoth <silvioukoth@gmail.com>
2022-06-24 16:22:04 -0700

    task 3

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,8 +4,8 @@
 int _putchar(char c);
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
-int main(int __attribute__((__unused__)) argc, char *argv[]);
-int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char __attribute__((unused)) argc, char *argv[]);
+int main(int argc, char __attribute__((unused)) argc, char *argv[]);
 int main(int argc, char __attribute__((unused)) argc, char *argv[]);
 
 #endif
