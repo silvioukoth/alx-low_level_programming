7f46027ecc56d121d28b0136b784c365f1a9f2b0
silvioukoth <silvioukoth@gmail.com>
2022-06-25 01:29:05 -0700

    task 0

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char __attribute__((unused)) argc, char *argv[]);
