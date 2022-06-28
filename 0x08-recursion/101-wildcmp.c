9ba6a49a71593c10745587572bf28e3f750e0685
silvioukoth <silvioukoth@gmail.com>
2022-06-28 06:02:36 -0700

    task101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -30,7 +30,7 @@ int strlen_no_wilds(char *str)
 
 /**
  * iterate_wild - Iterates through a string located at a wildcard
- * until it points to a non-wildcard character.
+ *                until it points to a non-wildcard character.
  *  @wildstr: The string to be iterated through.
  */
 void iterate_wild(char **wildstr)
@@ -44,13 +44,13 @@ void iterate_wild(char **wildstr)
 
 /**
  * postfix_match - Checks if a string str matches the postfix of
- * another string potentially containing wildcards.
+ *                 another string potentially containing wildcards.
  * @str: The string to be matched
  * @postfix: The postfix.
  *
  * Return: If str and postfix are identical - a pointer to the null byte
- * located at the end of postfix.
- * Otherwise - a pointer to the first unmatched character in postfix
+ *         located at the end of postfix.
+ *         Otherwise - a pointer to the first unmatched character in postfix
  */
 char *postfix_match(char *str, char *postfix)
 {
