7a531871adc049f0e753d5863e0ccc361617de48
silvioukoth <silvioukoth@gmail.com>
2022-06-28 06:32:24 -0700

    task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-
-int wildcmp(char *, char *);
+int strlen_no_wilds(char *str);
 void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
@@ -13,7 +12,7 @@ int wildcmp(char *s1, char *s2);
    *
    * Return: the length
    */
-int wildcmp(char *, char *)
+int strlen_no_wilds(char *str)
 {
 	int len = 0, index = 0;
 
