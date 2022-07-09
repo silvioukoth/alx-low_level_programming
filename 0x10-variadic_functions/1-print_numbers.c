61ebb1dfe5c36326ac5fc4077d328edf5e485f1f
silvioukoth <silvioukoth@gmail.com>
2022-07-09 03:49:43 -0700

    add task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(valist, int);
 		printf("%d", num);
-		if (i < n - 1 && separator)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
