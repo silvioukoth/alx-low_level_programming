f6fc49ad5351aea425946c368feaa7e87d942e59
silvioukoth <silvioukoth@gmail.com>
2022-07-19 02:59:55 -0700

    add task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 		}
 		if (current == NULL)
-			return (NUUL);
+			return (NULL);
 	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
