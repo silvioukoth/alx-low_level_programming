4f0a819671fa891c5ad0938bbd6aa9805f21b102
silvioukoth <silvioukoth@gmail.com>
2022-07-19 00:09:31 -0700

    add task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,19 +4,19 @@
 /**
  * stint_len - Returns the number of elements
  *             in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * @h: @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
+		count++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
