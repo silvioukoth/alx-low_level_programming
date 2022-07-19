1413a7f5b32931d663a4a330f92616256e2d0197
silvioukoth <silvioukoth@gmail.com>
2022-07-19 00:21:50 -0700

    nadd task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,16 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * stint_len - Returns the number of elements
- *             in a linked listint_t list.
- * @h: @h: A pointer to the head of the listint_t list.
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the linked list.
  *
- * Return: The number of elements in the listint_t list.
+ * Return: number of elements in a linked listint_t list
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while while (h != NULL)
 	{
 		count++;
 		h = h->next;
