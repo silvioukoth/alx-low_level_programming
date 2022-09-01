05372e4380d7816d40b99f7f95addd272318f8a0
silvioukoth <silvioukoth@gmail.com>
2022-09-01 06:54:58 -0700

    add task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	for (; index != 0; index--)
 	{
 		if (tmp == NULL)
-			return (NULL);
+			return (-1);
 		tmp = tmp->next;
 	}
 
