22f0f07147d4e0aa9f5e68bc5bd8dabb589b94ae
silvioukoth <silvioukoth@gmail.com>
2023-01-05 01:28:12 -0800

    addd first commits

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include search_algos.c
+
+/**
+ * linear_search - searches for a value in an array using linear search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of element in the array.
+ * @value: The value to search for in the array.
+ *
+ * Return: If the value is not present or is NULL, -1.
+ *         Otherwise, the first index where the value is located.
+ *
+ * Description: Prints a value every time itis comppplared in the array.
+ */
+int lieanre_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,19 @@
+This is about 0x1E. C - Search Algorithms.
+
+Mandatory Tasks:
+
+0-linear.c
+
+1-binary.c
+
+2-O
+
+3-O
+
+4-O
+
+5-O
+
+6-O
+
+Other nine advanced tasks.
